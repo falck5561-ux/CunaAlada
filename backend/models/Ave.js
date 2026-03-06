86b9d1246165379b29dd2093725b4b3c3769b6c0
falck5561-ux <falck5561@gmail.com>
2026-03-06 00:08:35 -0600

    Sprint 2: Estructura de modelos de datos para Agapornis

---
+++ backend/models/Ave.js
@@ -0,0 +1,38 @@
+// backend/models/Ave.js
+const mongoose = require('mongoose');
+
+const aveSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    mutacion: {
+        type: String, // Ej: Roseicollis, Fischeri, Azul D
+        required: true
+    },
+    fechaNacimiento: {
+        type: Date,
+        required: true // ¡ESTO ES LO MÁS IMPORTANTE PARA TU IDEA!
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    fotoUrl: {
+        type: String, // Aquí guardaremos el link de la imagen
+        default: "https://via.placeholder.com/300"
+    },
+    estado: {
+        type: String,
+        enum: ['DISPONIBLE', 'RESERVADO', 'VENDIDO'],
+        default: 'DISPONIBLE'
+    },
+    descripcion: {
+        type: String // Para poner notas como "Portador de azul"
+    }
+}, {
+    timestamps: true // Crea automáticamente campos de "creado el..."
+});
+
+module.exports = mongoose.model('Ave', aveSchema);
